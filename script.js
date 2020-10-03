0da8c5f7487361307d11f3f005caef4e12ee5be2
Shreyansh <69718041+Shreyansh2006@users.noreply.github.com>
2020-10-03 12:24:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var audio = document.createElement('audio');
-audio.setAttribute('src', 'C:/Users/nitac/Desktop/All Coding/Web Projects/sound.mp3'); 
+audio.setAttribute('src', 'sound.mp3'); 
 audio.loop=true;
 audio.play();
